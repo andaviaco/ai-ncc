2033f44f939c2b2919d159a3e9dc748d84d6d5b4
Andrés Ávila <andaviaco@gmail.com>
2017-11-28 00:28:16 -0600

    implement NCC function

--- aincc/main.py
+++ aincc/main.py
@@ -1,10 +1,30 @@
+import numpy as np
 from PIL import Image
 
 from devol import DE
 
 
-def sphere(vector):
-    return sum([x**2 for x in vector])
+def ncc(img, template):
+    ih, iw = img.shape
+    th, tw = template.shape
+
+    itemplate = template.astype('int_')
+    iimg = img.astype('int_')
+
+    t_sum = (itemplate ** 2).sum()
+
+    def fn(vector):
+        y, x = np.rint(vector).astype('int_')
+        i_slice = iimg[y:y+th, x:x+tw]
+        it_sum = (i_slice ** 2).sum()
+
+        a = (i_slice * itemplate).sum()
+        b = it_sum * t_sum
+        result = a / np.sqrt(b)
+
+        return result
+
+    return fn
 
 def main():
     target_im = Image.open('img/Image_1.bmp').convert("L")
